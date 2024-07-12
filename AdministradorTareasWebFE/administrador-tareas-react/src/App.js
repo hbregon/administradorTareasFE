05a00b5b2f1f7035595f51e5b75363a30ab9f6ae
hbrenesg <hbrenesg@ucenfotec.ac.cr>
2024-07-12 11:48:41 -0600

    creacion del Web react App

---
+++ AdministradorTareasWebFE/administrador-tareas-react/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Tabla from './componentes/tabla/Tabla';
+
+function App() {
+  return (
+    <Tabla/>
+     
+  );
+}
+
+export default App;
---
+++ AdministradorTareasWebFE/administrador-tareas-react/src/componentes/tabla/Tabla.js
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import DataTable from 'react-data-table-component'
+
+function Tabla() {
+    const columnas = [
+        {   
+            name: 'Descripción',
+            selector: fila => fila.descripcion
+        },
+        {
+            name: 'Colaborador',
+            selector: fila => fila.nombreColaborador
+        },
+        {
+            name: 'Estado',
+            selector: fila => fila.estado
+        },
+        {
+            name: 'Prioridad',
+            selector: fila => fila.prioridad
+        },
+        {
+            name: 'Fecha Inicio',
+            selector: fila => fila.fechaInicio
+        },
+        {
+            name: 'Fecha Fin',
+            selector: fila => fila.fechaFin
+        },
+    ];
+
+    const [resultado, setResultado] = useState([]);
+    obtenerListaTareas()
+    .then((respuestaTareas) => {
+        obtenerListaColaboradores()
+        .then((respuestaColaboradores) => {
+            for (let index = 0; index < respuestaTareas.length; index++) {
+                for (let index2 = 0; index2 < respuestaColaboradores.length; index2++) {
+                    if (respuestaTareas[index].idColaborador === respuestaColaboradores[index2].idColaborador) {
+                        respuestaTareas[index].nombreColaborador = respuestaColaboradores[index].nombre
+                    }
+                }
+            }
+            setResultado(respuestaTareas)
+        })
+        .catch((error) => console.log(error))
+    })
+
+    return (
+        <div>
+            <DataTable
+                columns={columnas}
+                noDataComponent = 'La tabla no tiene datos'
+                data={resultado}
+                title="Tareas"
+                defaultSortAsc={5}
+                />
+        </div>
+    )
+}
+
+function obtenerListaColaboradores() {
+    return new Promise((resuelta, rechazada) => {
+            fetch("https://administadortareasapi.azurewebsites.net/api/Colaborador/Lista")
+            .then((respuesta) => {
+                return respuesta.json();
+            })
+            .then((colaboradores) => {
+                resuelta(colaboradores.respuesta);
+            })
+            .catch((error) => rechazada(error));
+        }, []);
+}
+
+function obtenerListaTareas() {
+    return new Promise((resuelta, rechazada) => {
+        fetch("https://administadortareasapi.azurewebsites.net/api/Tarea/Lista")
+        .then((respuesta) => {
+            return respuesta.json();
+        })
+        .then((tareas) => {
+            resuelta(tareas.respuesta);
+        })
+        .catch((error) => rechazada(error));
+    }, []);
+}
+
+export default Tabla;
